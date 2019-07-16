31ee9cab6779f0b1f70ba44ab1b28fa1d0fbe5d6
Michael Simader <michael@simader.me>
2019-07-16 16:29:59 -0400

    answer eleven: group by and aggregate on continent level

--- Assignment 3/assignment3.py
+++ Assignment 3/assignment3.py
@@ -135,12 +135,38 @@ def answer_ten():
     median = Top15['% Renewable'].median()
     Top15['Above Mean'] = [1 if x >= median else 0 for x in Top15['% Renewable']]
     return Top15['Above Mean']
-
 answer_ten()
 
 # %%
 # Answer Eleven
 def answer_eleven():
+    Top15 = answer_one()
+    ContinentDict  = {'China':'Asia',
+                  'United States':'North America',
+                  'Japan':'Asia',
+                  'United Kingdom':'Europe',
+                  'Russian Federation':'Europe',
+                  'Canada':'North America',
+                  'Germany':'Europe',
+                  'India':'Asia',
+                  'France':'Europe',
+                  'South Korea':'Asia',
+                  'Italy':'Europe',
+                  'Spain':'Europe',
+                  'Iran':'Asia',
+                  'Australia':'Australia',
+                  'Brazil':'South America'}
+
+    Top15['Continent'] = [ContinentDict[country] for country in Top15.index]
+    Top15['Population'] = Top15['Energy Supply'] / Top15['Energy Supply per Capita']
+
+    df = Top15.set_index('Continent').groupby(level=0)['Population'].agg({
+        'size' : np.size,
+        'sum' : np.sum,
+        'mean' : np.mean,
+        'std' : np.std
+    })
+    return df
 
 answer_eleven()
 
