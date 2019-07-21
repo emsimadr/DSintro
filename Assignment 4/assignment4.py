d4d5cc3a604d0577868773cd86938ebd4b0e3f12
Michael Simader <michael@simader.me>
2019-07-21 15:48:29 -0400

    Read university towns

---
+++ Assignment 4/assignment4.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import re
+from scipy.stats import ttest_ind
+
+path='G:\\My Drive\\Education\\Coursera\\Introduction to Data Science with Python\\DSIntro\\Assignment 4\\'
+
+# Use this dictionary to map state names to two letter acronyms
+states = {'OH': 'Ohio', 'KY': 'Kentucky', 'AS': 'American Samoa', 'NV': 'Nevada', 'WY': 'Wyoming', 'NA': 'National', 'AL': 'Alabama', 'MD': 'Maryland', 'AK': 'Alaska', 'UT': 'Utah', 'OR': 'Oregon', 'MT': 'Montana', 'IL': 'Illinois', 'TN': 'Tennessee', 'DC': 'District of Columbia', 'VT': 'Vermont', 'ID': 'Idaho', 'AR': 'Arkansas', 'ME': 'Maine', 'WA': 'Washington', 'HI': 'Hawaii', 'WI': 'Wisconsin', 'MI': 'Michigan', 'IN': 'Indiana', 'NJ': 'New Jersey', 'AZ': 'Arizona', 'GU': 'Guam', 'MS': 'Mississippi', 'PR': 'Puerto Rico', 'NC': 'North Carolina', 'TX': 'Texas', 'SD': 'South Dakota', 'MP': 'Northern Mariana Islands', 'IA': 'Iowa', 'MO': 'Missouri', 'CT': 'Connecticut', 'WV': 'West Virginia', 'SC': 'South Carolina', 'LA': 'Louisiana', 'KS': 'Kansas', 'NY': 'New York', 'NE': 'Nebraska', 'OK': 'Oklahoma', 'FL': 'Florida', 'CA': 'California', 'CO': 'Colorado', 'PA': 'Pennsylvania', 'DE': 'Delaware', 'NM': 'New Mexico', 'RI': 'Rhode Island', 'MN': 'Minnesota', 'VI': 'Virgin Islands', 'NH': 'New Hampshire', 'MA': 'Massachusetts', 'GA': 'Georgia', 'ND': 'North Dakota', 'VA': 'Virginia'}
+
+def get_list_of_university_towns():
+    with open(path + 'university_towns.txt', 'r') as f:
+        x = f.readlines()
+        x = [word[:-1] for word in x]
+
+    pairs = []
+    cur_state = ''
+    for place in x:
+        if place[-6:] == '[edit]':
+            cur_state = place[:-6]
+        else:
+            place = re.sub(r"\s\(.+",'',place)
+            pairs.append([cur_state,place])
+    print(pairs)
+    return None
+
+get_list_of_university_towns()
